56e560fcdd99c688de5a4ccfe305434f35e72018
Gabriel <gabrielback.dev@gmail.com>
2026-08-02 17:46:33 -0300

    Permite migrar banco novo sem tabela empresa

--- db/migrations/012_update_empresa_contact.sql
+++ db/migrations/012_update_empresa_contact.sql
@@ -1,4 +1,7 @@
-UPDATE empresa
+DO $$
+BEGIN
+  IF to_regclass('public.empresa') IS NOT NULL THEN
+    UPDATE empresa
 SET
   nome_fantasia = 'SCX Laser',
   razao_social = 'DANIEL WOLFF RIBEIRO',
@@ -22,6 +25,8 @@ WHERE id = (
   WHERE ativo = true
   ORDER BY id ASC
   LIMIT 1
-);
+    );
+  END IF;
+END $$;
 
 DROP TABLE IF EXISTS scx_site_settings;
